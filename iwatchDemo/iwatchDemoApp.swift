9a52e10e6aeeb4ff432e9bc238e2d8ee40bbf473
vincent <985801126@qq.com>
2023-03-31 12:00:54 +0800

    Initial Commit

---
+++ iwatchDemo/iwatchDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iwatchDemoApp.swift
+//  iwatchDemo
+//
+//  Created by test on 2023/3/31.
+//
+
+import SwiftUI
+
+@main
+struct iwatchDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
